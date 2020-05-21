1b7200d447f816db42d9255e8f8db031cdbcec69
Jeremiah Damiron <jeremiahdamiron@gmail.com>
2020-05-21 13:25:18 -0400

    Update email form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,7 +7,6 @@ class Contact extends Component {
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
       var phone = this.props.data.phone;
-      var email = this.props.data.email;
       var message = this.props.data.contactmessage;
     }
 
@@ -32,70 +31,87 @@ class Contact extends Component {
               method="POST"
               id="contactForm"
               name="contactForm"
+              onSubmit={this.clearForm}
             >
               <input type="hidden" name="form-name" value="contactForm" />
-              <fieldset>
-                <div>
-                  <label htmlFor="contactName">
-                    Name <span className="required">*</span>
-                  </label>
-                  <input
-                    type="text"
-                    defaultValue=""
-                    size="35"
-                    id="contactName"
-                    name="contactName"
-                    onChange={this.handleChange}
-                  />
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label htmlFor="name">
+                      Name <span className="required">*</span>
+                    </label>
+                    <input
+                      className="form-control"
+                      id="name"
+                      name="name"
+                      type="text"
+                      placeholder="Your Name *"
+                      required="required"
+                      data-validation-required-message="Please enter your name."
+                      onChange={this.handleChange}
+                    />
+                    <p className="help-block text-danger"></p>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="email">
+                      Email <span className="required">*</span>
+                    </label>
+                    <input
+                      className="form-control"
+                      id="email"
+                      name="email"
+                      type="email"
+                      placeholder="Your Email *"
+                      required
+                      data-validation-required-message="Please enter your email address."
+                      onChange={this.handleChange}
+                    />
+                    <p className="help-block text-danger"></p>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="reason">Reason</label>
+                    <input
+                      className="form-control"
+                      id="reason"
+                      name="reason"
+                      type="text"
+                      placeholder="Reason for contact *"
+                      required
+                      data-validation-required-message="Please enter your reason for contacting."
+                      onChange={this.handleChange}
+                    />
+                    <p className="help-block text-danger"></p>
+                  </div>
                 </div>
-
-                <div>
-                  <label htmlFor="contactEmail">
-                    Email <span className="required">*</span>
-                  </label>
-                  <input
-                    type="text"
-                    defaultValue=""
-                    size="35"
-                    id="contactEmail"
-                    name="contactEmail"
-                    onChange={this.handleChange}
-                  />
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label htmlFor="message">
+                      Message <span className="required">*</span>
+                    </label>
+                    <textarea
+                      className="form-control"
+                      id="message"
+                      name="message"
+                      placeholder="Your Message *"
+                      required
+                      data-validation-required-message="Please enter a message."
+                      onChange={this.handleChange}
+                    ></textarea>
+                    <p className="help-block text-danger"></p>
+                  </div>
                 </div>
-
-                <div>
-                  <label htmlFor="contactSubject">Subject</label>
-                  <input
-                    type="text"
-                    defaultValue=""
-                    size="35"
-                    id="contactSubject"
-                    name="contactSubject"
-                    onChange={this.handleChange}
-                  />
-                </div>
-
-                <div>
-                  <label htmlFor="contactMessage">
-                    Message <span className="required">*</span>
-                  </label>
-                  <textarea
-                    cols="50"
-                    rows="15"
-                    id="contactMessage"
-                    name="contactMessage"
-                  ></textarea>
-                </div>
-
-                <div>
-                  <button type="submit" className="submit">
-                    Submit
+                <div className="clearfix"></div>
+                <div className="col-lg-12 text-center">
+                  <div id="success"></div>
+                  <button
+                    id="sendMessageButton"
+                    className="btn btn-primary btn-xl text-uppercase"
+                    type="submit"
+                  >
+                    Send Message
                   </button>
-                  <span id="image-loader">
-                    <img alt="" src="images/loader.gif" />
-                  </span>
                 </div>
-              </fieldset>
+              </div>
             </form>
 
             <div id="message-warning"> Error boy</div>
@@ -115,35 +131,6 @@ class Contact extends Component {
                 <br />
                 <span>{phone}</span>
               </p>
-            </div>
-
-            <div className="widget widget_tweets">
-              <h4 className="widget-title">Latest Tweets</h4>
-              <ul id="twitter">
-                <li>
-                  <span>
-                    This is Photoshop's version of Lorem Ipsum. Proin gravida
-                    nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
-                    quis bibendum auctor, nisi elit consequat ipsum
-                    <a href="#">http://t.co/CGIrdxIlI3</a>
-                  </span>
-                  <b>
-                    <a href="#">2 Days Ago</a>
-                  </b>
-                </li>
-                <li>
-                  <span>
-                    Sed ut perspiciatis unde omnis iste natus error sit
-                    voluptatem accusantium doloremque laudantium, totam rem
-                    aperiam, eaque ipsa quae ab illo inventore veritatis et
-                    quasi
-                    <a href="#">http://t.co/CGIrdxIlI3</a>
-                  </span>
-                  <b>
-                    <a href="#">3 Days Ago</a>
-                  </b>
-                </li>
-              </ul>
             </div>
           </aside>
         </div>
